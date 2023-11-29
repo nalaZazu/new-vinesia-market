2974dd334187f45fb91de65039d86c93f1c0d534
uswanadeem09675 <wtsplayer001@gamil.com>
2023-11-29 17:18:51 +0500

    critics-section-done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,31 +5,35 @@ import Subscription from "../components/subscription/page";
 import VideoBanner from "../components/videoBanner/page";
 import WineArt from "../components/wineArt/page";
 import HeritageCarousel from "../components/heritageCarousel/page";
+import ExploreRegion from "../components/exploreRegion/page";
 
 export default function Home() {
   return (
     <>
-      <Banner />
-      <div className=" pt-40 container mx-auto">
-        <h2 className="text-center text-zinc-800 text-7xl font-normal leading-[80px] pb-16 font-['Canela']">
-          Invest in heritage and excellence
-        </h2>
-        <ProductCards />
-        <div className="pt-20 pb-40">
-          <VideoBanner />
+      <div className="bg-orange-50">
+        <Banner />
+        <div className=" pt-40 container mx-auto ">
+          <h2 className="text-center text-zinc-800 text-7xl font-normal leading-[80px] pb-16 font-['Canela']">
+            Invest in heritage and excellence
+          </h2>
+          <ProductCards />
+          <div className="pt-20 pb-40">
+            <VideoBanner />
+          </div>
         </div>
+        <Subscription />
+        <div className=" pt-40 pb-44 container mx-auto">
+          <h1 className="text-primary font-semibold text-3xl leading-10">
+            Critics selection
+          </h1>
+          <Critics />
+        </div>
+        <div className=" pt-40 pb-44 bg-red-900 relative">
+          <WineArt />
+        </div>
+        <HeritageCarousel />
+        <ExploreRegion />
       </div>
-      <Subscription />
-      <div className=" pt-40 pb-44 container mx-auto">
-        <h1 className="text-primary font-semibold text-3xl leading-10">
-          Critics selection
-        </h1>
-        {/* <Critics /> */}
-      </div>
-      <div className=" pt-40 pb-44 bg-red-900 relative">
-        <WineArt />
-      </div>
-      <HeritageCarousel />
     </>
   );
 }
---
+++ src/constants/regionexplor.ts
@@ -0,0 +1,14 @@
+export const regionexplor = [
+  { id: 1, name: "Bordeaux", href: "/" },
+  { id: 2, name: "Burgundy", href: "/" },
+  { id: 3, name: "Champagne", href: "/" },
+  { id: 4, name: "Rhone", href: "/" },
+  { id: 5, name: "Alsace", href: "/" },
+];
+
+export const regionItaly = [
+  { id: 1, name: "Tuscany", href: "/" },
+  { id: 2, name: "Piedmont", href: "/" },
+];
+
+export const regionUs = [{ id: 1, name: "California", href: "/" }];
