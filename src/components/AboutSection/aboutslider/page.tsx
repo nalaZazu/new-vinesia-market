044dbd0de2faba103918181745b2345c2ba44eb9
uswanadeem09675 <wtsplayer001@gamil.com>
2023-12-13 22:50:40 +0500

    about-page-done

--- src/components/AboutSection/aboutslider/page.tsx
+++ src/components/AboutSection/aboutslider/page.tsx
@@ -3,7 +3,7 @@ import React, { useRef, useState } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { Arrows, NextIcon, PrevIcon } from "../../assets/icons/Icons";
+import { Arrows, NextIcon, NextIcon2, PrevIcon, PrevIcon2 } from "../../../assets/icons/Icons";
 import sliderabout from "@/assets/images/sliderImage.png";
 import Image from "next/image";
 import shareIcon from "@/assets/icons/share.svg";
@@ -37,6 +37,24 @@ const AboutSlider = () => {
         },
       },
     ],
+    // prevArrow: (
+    //   <div className=" productcarousel z-50">
+    //     <div className=" h-20 text-secondary w-6 text-center flex items-center absolute lg:top-[230px] lg:left-[40px] md:top-[215px] md:left-[0px] top-[0px] left-[0px] ">
+    //       <div className="">
+    //         <PrevIcon2 />
+    //       </div>
+    //     </div>
+    //   </div>
+    // ),
+    // nextArrow: (
+    //   <div className="productcarousel  z-50 ">
+    //     <div className="text-secondary h-20 w-6 text-center flex items-center absolute lg:top-[230px] lg:right-[40px] md:top-[215px] md:right-[345px] top-[0px] right-[0px]">
+    //       <div className="">
+    //         <NextIcon2/>
+    //       </div>
+    //     </div>
+    //   </div>
+    // ),
   };
   return (
     <React.Fragment>
@@ -47,7 +65,7 @@ const AboutSlider = () => {
             <Arrows storke="#FFFFFF" />
           </button>
           <div className="flex justify-end gap-4 pe-20">
-            <div className="productcarousel  z-50">
+            <div className="productcarousel  z-auto">
               <div
                 onClick={() => slider?.current?.slickPrev()}
                 className={`text-black w-16 h-12 flex items-center rounded-full border  justify-center cursor-pointer  ${
@@ -92,25 +110,25 @@ const AboutSlider = () => {
           {...sliderSettings}
           className="invest_carousel z-0  "
         >
-
           {slideData?.map((item: any, i: any) => {
             return (
-              <div key={i} className="  pt-8">
+              <div key={i} className="px-8  pt-8">
                 <div className="">
-                  <div className="relative">
-                    <Image src={sliderabout} alt="image" />
-                    <div className="absolute bottom-24 -left-6">
+                  <div className="relative  md:h-[246.23px] h-[165.61px] bg-no-repeat  bg-center bg-[url('https://i.ibb.co/LCb3y9J/slider-Image.png')]">
+                    {/* <Image src={sliderabout} alt="image" /> */}
+                    <div className="absolute bottom-7 -left-6">
+                      {/* absolute bottom-24 -left-6 */}
                       <div className="max-w-[121px] h-[0px] border border-orange-700"></div>
                       <p className="text-white text-lg font-semibold  leading-relaxed">
                         Video title would come here
                       </p>
                     </div>
-                    <div className="pt-[15px] flex justify-end gap-4">
-                      <p className="text-white text-[13px] font-normal   uppercase tracking-tight">
-                        share
-                      </p>
-                      <Image src={shareIcon} alt="shareicon" />
-                    </div>
+                  </div>{" "}
+                  <div className="pt-[15px] flex justify-end gap-4 items-center">
+                    <p className="text-white text-[13px] font-normal   uppercase tracking-tight">
+                      share
+                    </p>
+                    <Image src={shareIcon} alt="shareicon" />
                   </div>
                 </div>
               </div>
