319be249b8504ca5f249e618764e46476c9b22a4
tauqeer180 <tauqeermalik855@gmail.com>
2023-12-19 07:53:48 +0500

    Merge Uswa

--- src/components/products/page.tsx
+++ src/components/products/page.tsx
@@ -29,7 +29,7 @@ const Product = ({ items }: { items?: ProductCardDto[] }) => {
             <>
               <div
                 key={i}
-                className="container mx-auto grid grid-cols-1 md:grid-cols-4 mt-16 gap-x-6 gap-y-10"
+                className="grid xl:grid-cols-4  lg:grid-cols-3 gap-8 md:grid-cols-2 grid-cols-1 pt-20"
               >
                 {item === undefined ? (
                   <></>
