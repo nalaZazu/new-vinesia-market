050ebb9c597603dcb70d2159bd243069c0a30794
tauqeer180 <tauqeermalik855@gmail.com>
2023-12-20 18:32:46 +0500

    Build Fix

--- src/context/user.tsx
+++ src/context/user.tsx
@@ -1,5 +1,14 @@
+"use client";
 import { createContext, useContext, useState } from "react";
-import { Connector, WagmiConfig, configureChains, createConfig, useAccount, useConnect, useDisconnect } from "wagmi";
+import {
+  Connector,
+  WagmiConfig,
+  configureChains,
+  createConfig,
+  useAccount,
+  useConnect,
+  useDisconnect,
+} from "wagmi";
 import { goerli, mainnet } from "wagmi/chains";
 import { CoinbaseWalletConnector } from "wagmi/connectors/coinbaseWallet";
 import { MetaMaskConnector } from "wagmi/connectors/metaMask";
@@ -8,155 +17,180 @@ import { publicProvider } from "wagmi/providers/public";
 import { LoginType, MagicCustomConnector } from "./connectors/MagicConnector";
 
 export interface ProvideUser {
-    status: string
-    address: `0x${string}` | undefined
-    isConnected: boolean
-    isConnecting: boolean
-    isReconnecting: boolean
-    isDisconnected: boolean
-    isLoading: boolean
-    connectors: Connector<any, any>[]
+  status: string;
+  address: `0x${string}` | undefined;
+  isConnected: boolean;
+  isConnecting: boolean;
+  isReconnecting: boolean;
+  isDisconnected: boolean;
+  isLoading: boolean;
+  connectors: Connector<any, any>[];
 
-    currency: string
-    language: string
+  currency: string;
+  language: string;
 
-    getText: (text: string) => string
-    getPriceText: (price: number) => string
-    getPriceDifference: (release: number, current: number) => string
+  getText: (text: string) => string;
+  getPriceText: (price: number) => string;
+  getPriceDifference: (release: number, current: number) => string;
 
-    // connectAsync: (args?: Partial<ConnectArgs>) => Promise<ConnectResult<_wagmi_core.PublicClient>>;
-    disconnectAsync: () => Promise<any>
+  // connectAsync: (args?: Partial<ConnectArgs>) => Promise<ConnectResult<_wagmi_core.PublicClient>>;
+  disconnectAsync: () => Promise<any>;
 }
 
 export function useProvideUser(): ProvideUser {
-  const { status, address, isConnected, isConnecting, isReconnecting, isDisconnected } = useAccount()
-  const { connectAsync, connectors, data, isLoading } = useConnect()
-  const { disconnectAsync } = useDisconnect()
+  const {
+    status,
+    address,
+    isConnected,
+    isConnecting,
+    isReconnecting,
+    isDisconnected,
+  } = useAccount();
+  const { connectAsync, connectors, data, isLoading } = useConnect();
+  const { disconnectAsync } = useDisconnect();
 
-    const [currency, setCurrency] = useState('EUR')
-    const [language, setLanguage] = useState('en')
+  const [currency, setCurrency] = useState("EUR");
+  const [language, setLanguage] = useState("en");
 
-    function getText(text: string): string {
-        return ''
-    }
+  function getText(text: string): string {
+    return "";
+  }
 
-    function getPriceText(price: number): string {
-        const priceDec = price/100
+  function getPriceText(price: number): string {
+    const priceDec = price / 100;
 
-        return priceDec.toLocaleString('en-US', { style: "currency", currency: "EUR" })
-    }
+    return priceDec.toLocaleString("en-US", {
+      style: "currency",
+      currency: "EUR",
+    });
+  }
 
-    function getPriceDifference(release: number, current: number) {
-
-              {/* +
+  function getPriceDifference(release: number, current: number) {
+    {
+      /* +
               {`  ${(
                 ((release?.highPrice - release?.releasePrice) /
                   release?.releasePrice) *
                 100
-              ).toFixed(3)} % ↑`} */}
-            //   + 66.6%
-
-        const diff = (current - release) / release * 100
-
-        return diff.toFixed(1)+'%'
+              ).toFixed(3)} % ↑`} */
     }
+    //   + 66.6%
 
+    const diff = ((current - release) / release) * 100;
 
-    return {
-        currency,
-        language,
+    return diff.toFixed(1) + "%";
+  }
 
-        getText,
+  return {
+    currency,
+    language,
 
-        getPriceText,
-        getPriceDifference,
+    getText,
 
-        status,
-        address,
-        isConnected,
-        isConnecting,
-        isReconnecting,
-        isDisconnected,
-        isLoading,
-        connectors,
-        // connectAsync,
-        disconnectAsync
-    }
+    getPriceText,
+    getPriceDifference,
+
+    status,
+    address,
+    isConnected,
+    isConnecting,
+    isReconnecting,
+    isDisconnected,
+    isLoading,
+    connectors,
+    // connectAsync,
+    disconnectAsync,
+  };
 }
 
-
 const { chains, publicClient, webSocketPublicClient } = configureChains(
-    [mainnet, ...(process.env.NODE_ENV === 'development' ? [goerli] : [])],
-    [
-        publicProvider(),
-    ],
-)
-
-
+  [mainnet, ...(process.env.NODE_ENV === "development" ? [goerli] : [])],
+  [publicProvider()]
+);
 
 export const config = createConfig({
-    autoConnect: true,
-    connectors: [
-        new MetaMaskConnector({ chains }),
-        new CoinbaseWalletConnector({
-            chains,
-            options: {
-                appName: 'wagmi',
-            },
-        }),
-        new WalletConnectConnector({
-            chains,
-            options: {
-                projectId: process.env.NEXT_PUBLIC_API_WALLET_CONNECT,
-            },
-        }),
+  autoConnect: true,
+  connectors: [
+    new MetaMaskConnector({ chains }),
+    new CoinbaseWalletConnector({
+      chains,
+      options: {
+        appName: "wagmi",
+      },
+    }),
+    new WalletConnectConnector({
+      chains,
+      options: {
+        projectId: process.env.NEXT_PUBLIC_API_WALLET_CONNECT,
+      },
+    }),
 
-        new MagicCustomConnector({
-            options: {
-                apiKey: process.env.NEXT_PUBLIC_API_MAGIC_LINK, //required
-            },
-        }, LoginType.Social, 'google'),
-        new MagicCustomConnector({
-            options: {
-                apiKey: process.env.NEXT_PUBLIC_API_MAGIC_LINK, //required
-            },
-        }, LoginType.Social, 'twitter'),
-        new MagicCustomConnector({
-            options: {
-                apiKey: process.env.NEXT_PUBLIC_API_MAGIC_LINK, //required
-            },
-        }, LoginType.Social, 'apple'),
-        new MagicCustomConnector({
-            options: {
-                apiKey: process.env.NEXT_PUBLIC_API_MAGIC_LINK, //required
-            },
-        }, LoginType.Social, 'facebook')
+    new MagicCustomConnector(
+      {
+        options: {
+          apiKey: process.env.NEXT_PUBLIC_API_MAGIC_LINK, //required
+        },
+      },
+      LoginType.Social,
+      "google"
+    ),
+    new MagicCustomConnector(
+      {
+        options: {
+          apiKey: process.env.NEXT_PUBLIC_API_MAGIC_LINK, //required
+        },
+      },
+      LoginType.Social,
+      "twitter"
+    ),
+    new MagicCustomConnector(
+      {
+        options: {
+          apiKey: process.env.NEXT_PUBLIC_API_MAGIC_LINK, //required
+        },
+      },
+      LoginType.Social,
+      "apple"
+    ),
+    new MagicCustomConnector(
+      {
+        options: {
+          apiKey: process.env.NEXT_PUBLIC_API_MAGIC_LINK, //required
+        },
+      },
+      LoginType.Social,
+      "facebook"
+    ),
 
-        // new DedicatedWalletConnector({
-        //   options: {
-        //     apiKey: magicLinkApiKey, //required
-        //     oauthOptions : {
-        //       providers: ['google', 'twitter', 'apple'],          
-        //     },
-        //     enableSMSLogin: true,
-        //     enableEmailLogin: true,
-        //     customHeaderText: 'Vinesia Login'
-        //     //...Other options
-        //   },
-        // })
-    ],
-    publicClient,
-    webSocketPublicClient,
-})
+    // new DedicatedWalletConnector({
+    //   options: {
+    //     apiKey: magicLinkApiKey, //required
+    //     oauthOptions : {
+    //       providers: ['google', 'twitter', 'apple'],
+    //     },
+    //     enableSMSLogin: true,
+    //     enableEmailLogin: true,
+    //     customHeaderText: 'Vinesia Login'
+    //     //...Other options
+    //   },
+    // })
+  ],
+  publicClient,
+  webSocketPublicClient,
+});
 
 const userContext = createContext<ProvideUser | null>(null);
 
 export function UserProvider({ children }: { children: React.ReactNode }) {
-    return (<userContext.Provider value={useProvideUser()}>{children}</userContext.Provider>);
+  return (
+    <userContext.Provider value={useProvideUser()}>
+      {children}
+    </userContext.Provider>
+  );
 }
 
 export function useUserContext(): ProvideUser {
-    const context = useContext(userContext);
-    if (context === null) throw new Error('User provider is not set')
-    return context
+  const context = useContext(userContext);
+  if (context === null) throw new Error("User provider is not set");
+  return context;
 }
