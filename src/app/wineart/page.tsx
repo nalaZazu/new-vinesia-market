5df50e373352b2a3bd6b6af4b6b62fe2198772ff
Hsn abdullah <129732338+hsnabdullah@users.noreply.github.com>
2023-12-21 12:46:01 +0500

    artists section done

--- src/app/wineart/page.tsx
+++ src/app/wineart/page.tsx
@@ -1,7 +1,8 @@
+import React from "react";
 import Wanttoknow from "@/components/aboutpage/Wanttoknow";
 import Relationship from "@/components/wineArtPage/Relationship";
 import WineryBanner from "@/components/wineArtPage/WineryBanner";
-import React from "react";
+import Artists from "@/components/wineArtPage/Artists";
 
 export default function wineryArt() {
   return (
@@ -19,6 +20,21 @@ export default function wineryArt() {
       <div className="py-12 bg-red-900">
         <div className=" container mx-auto px-4">
           <Wanttoknow />
+        </div>
+      </div>
+      <div className="pt-32 pb-40">
+        <div className="container mx-auto px-4">
+          <h2 className="text-zinc-800 text-7xl font-normal leading-[84px] pb-12">
+            Artists we have partnered with 
+          </h2>
+          <Artists />
+        </div>
+      </div>
+      <div className="pb-40">
+        <div className="container mx-auto px-4">
+          <h2 className="text-center text-zinc-800 text-7xl font-normal leading-[84px] tracking-[-1.44]">
+            Selected wineries represented in our Collections 
+          </h2>
         </div>
       </div>
     </div>
