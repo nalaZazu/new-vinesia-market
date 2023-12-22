1acb5fc49444683e0e65c1bc510ee96eb4032dec
tauqeer180 <tauqeermalik855@gmail.com>
2023-12-22 14:39:30 +0500

    Invest Filters

--- src/app/invest/page.tsx
+++ src/app/invest/page.tsx
@@ -2,7 +2,7 @@
 
 import InvestBread from "@/common/InvestBread";
 import xmark from "../../assets/icons/x-mark-anim.svg";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import Dropdown from "@/common/DropDown/page";
 import MobileFilter from "./moibleview/page";
@@ -12,18 +12,6 @@ import NewsletterMobile from "@/components/newsletter/MobileView";
 import Product from "@/components/products/page";
 import useSWR from "swr";
 
-const fetcher = async (url: string, payload?: string) => {
-  const options = {
-    method: "POST",
-    ...(payload && { body: payload }),
-    headers: {
-      accept: "application/json",
-      "Content-Type": "application/json",
-    },
-  };
-
-  return fetch(url, options).then((res) => res.json());
-};
 import { MarksAnim, Xmark } from "@/assets/icons/Icons";
 import ProductCards from "@/components/productCard/page";
 import FilterSection from "@/components/FilterSection/page";
@@ -31,11 +19,28 @@ import FilterSection from "@/components/FilterSection/page";
 const Invest = () => {
   const [tags, setTags] = useState<String[]>([]);
   const [selectedFilters, setSelectedFilters] = useState([]);
+  const fetcher = async (url: string, payload?: string) => {
+    const options = {
+      method: "POST",
+      ...(selectedFilters && {
+        body: JSON.stringify({ filters: selectedFilters }),
+      }),
+      headers: {
+        accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    };
+    console.log("fetch", url, payload);
+    return fetch(url, options).then((res) => res.json());
+  };
 
-  const { data, error, isLoading } = useSWR(
+  const { data, error, isLoading, mutate } = useSWR(
     `${process.env.NEXT_PUBLIC_API_ADDRESS}products/search/`,
     fetcher
   );
+  useEffect(() => {
+    mutate();
+  }, [selectedFilters]);
 
   return (
     <div>
@@ -56,7 +61,10 @@ const Invest = () => {
             </h2>
           </div>
 
-          <FilterSection selectedFilters={selectedFilters} setSelectedFilters={setSelectedFilters} />
+          <FilterSection
+            selectedFilters={selectedFilters}
+            setSelectedFilters={setSelectedFilters}
+          />
 
           {/* product list  */}
           <div className="md:block hidden">
