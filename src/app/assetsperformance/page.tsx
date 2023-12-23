982fd4330345b0d68deae590a6944ad3979d30b2
Hsn abdullah <129732338+hsnabdullah@users.noreply.github.com>
2023-12-23 18:22:55 +0500

    assets done

--- src/app/assetsperformance/page.tsx
+++ src/app/assetsperformance/page.tsx
@@ -1,9 +1,18 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import AssetsBanner from "@/components/AssetsPerformance/AssetsBanner";
 import InvestorsLove from "@/components/AssetsPerformance/InvestorsLove";
+import ChartsSection from "@/components/AssetsPerformance/ChartsSection";
+import inflationImage from "@/assets/images/inflationimage.png";
 import Appactivity from "@/components/charts/page";
+import { Arrows, NextIcon } from "@/assets/icons/Icons";
+import TestimonialsSection from "@/components/AssetsPerformance/TestimonialsSection";
+import AboutSlider from "@/components/AboutSection/aboutslider/page";
+import Footer from "@/components/footer/page";
+import { VideoSlider } from "@/constants/videoslider";
+import NewslettersTwo from "@/components/newslttersTwo/page";
+import { AccordionWineAbout } from "@/components/accordion/page";
 
 export default function AssetsPerformance() {
   return (
@@ -22,33 +31,182 @@ export default function AssetsPerformance() {
 
       <div className="py-28 bg-[#7D302E]">
         <div className="container mx-auto px-4">
-          <h3 className="text-white text-4xl font-light leading-[44px] pb-6">
-            10 years. 120% increase.
-          </h3>
-          <div className="grid grid-cols-2 gap-8">
-            <p className="max-w-[608px] text-red-300 text-base font-normal leading-snug tracking-[-0.32px]">
-              That’s been the history of fine wine as an asset. While investing
-              in wine carries risks, just like other investments,  history
-              proves it has the potential for substantially high returns.
-            </p>
-            <p className="max-w-[608px] text-red-300 text-base font-normal leading-snug tracking-[-0.32px]">
-              Fine wine value grows over time as it matures and becomes rarer.
-              It has shown consistent positive performance over long periods and
-              is resilient during economic downturns. That has been the
-              historical performance of fine wine as an asset class (2010-2020).
-            </p>
-          </div>
-          <div className="grid grid-cols-2 gap-8 pt-10">
-            <div className="bg-[#F9F5EB] p-6">
-              <Appactivity />
-            </div>
+          <ChartsSection />
+        </div>
+      </div>
 
-            <div className="bg-[#F9F5EB] p-6">
+      <div className="py-40">
+        <div className="container mx-auto px-4">
+          <div className="grid grid-cols-2 max-w-[1140px] pb-40">
+            <div>
+              <Image src={inflationImage} alt="" />
+            </div>
+            <div className="ps-[139px]">
+              <div className="pb-20">
+                <h3 className="text-zinc-800 text-4xl font-light leading-[44px]">
+                  Inflation resilience
+                </h3>
+                <div className="py-8">
+                  <p className=" text-neutral-600 text-base font-normal leading-snug">
+                    Historically, when inflation increases the prices of goods
+                    and services, the value of wine tends to rise in tandem,
+                    thus fighting the effects of inflation. 
+                  </p>
+                </div>
+              </div>
+              <div>
+                <h3 className="text-zinc-800 text-4xl font-light leading-[44px]">
+                  Sustained global demand, scarce supply
+                </h3>
+                <div className="py-8">
+                  <p className=" text-neutral-600 text-base font-normal leading-snug">
+                    Historically, when inflation increases the prices of goods
+                    and services, the value of wine tends to rise in tandem,
+                    thus fighting the effects of inflation. 
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="grid grid-cols-2">
+            <div className="pe-[139px] flex items-end">
+              <div>
+                <h3 className="text-zinc-800 text-4xl font-light leading-[44px]">
+                  Low correlation with traditional markets
+                </h3>
+                <div className="py-8">
+                  <p className=" text-neutral-600 text-base font-normal leading-snug">
+                    While stock markets can be highly unpredictable and
+                    unstable, fine wine as an asset maintains steady returns
+                    during market downturns and through recessions. 
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="bg-[#F9F5EB] p-8">
               <Appactivity />
             </div>
           </div>
         </div>
       </div>
+
+      <div className="py-40 bg-[#F7EFDF]">
+        <div className="container mx-auto px-4">
+          <div className="grid md:grid-cols-2 grid-cols-1">
+            <div className="pe-[139px]">
+              <h2 className=" text-zinc-800 md:text-7xl font-normal  md:leading-[84px] md:tracking-[-1.44px] text-4xl leading-[42px] tracking-[-0.36px]">
+                The potential of wine investment in numbers
+              </h2>
+              <p className="max-w-[329px] text-zinc-800 text-lg font-semibold  md:pt-[72px] pt-6 pb-6 leading-relaxed   tracking-[-0.36px]">
+                See what your portfolio could have returned with wine
+              </p>
+
+              <button
+                // onClick={() => setVisible(true)}
+                className="px-8 py-4 text-white  bg-orange-700 rounded-[48px] justify-center items-center gap-3 inline-flex"
+              >
+                CALCULATE{" "}
+                <span>
+                  <Arrows storke="#FFFFFF" />
+                </span>
+              </button>
+            </div>
+            <div className="md:max-w-[608px] ">
+              <div className="bg-white p-4 h-[400px]">
+                <h5 className="text-zinc-800 text-lg font-semibold  leading-relaxed pb-3 tacking-[-0.36px]">
+                  Performance of investment portfolio with fine wine in
+                  comparison to baseline
+                </h5>
+                <div className="bg-orange-100 h-64">
+                  <div className="pt-1">
+                    <Appactivity />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="py-40">
+        <div className="container mx-auto px-4">
+          <TestimonialsSection />
+        </div>
+      </div>
+
+      <div className="py-28 bg-[#7D302E]">
+        <div className="container mx-auto px-4 flex flex-col items-center">
+          <div className="text-center pb-10">
+            <span className="text-orange-100 text-4xl font-light leading-[44px]">
+              Start your wine investing journey with{" "}
+            </span>
+            <p className="text-red-900 text-4xl font-light leading-[44px]">
+              Vinesia
+            </p>
+            <span className="text-center text-stone-400 text-base font-normal leading-snug">
+              Let us help you add this unique asset to your portfolio. 
+            </span>
+          </div>
+          <div className="text-center text-white text-xs font-normal uppercase leading-[18px] tracking-tight w-44 h-[68px] px-8 py-[22px] rounded-[48px] border border-white border-opacity-20 justify-center items-center gap-3 inline-flex">
+            Invest now
+            <NextIcon fill={"#fff"} />
+          </div>
+        </div>
+      </div>
+
+      {/* The faster way to get to know us */}
+      <div className="bg-[#2F222B]">
+        <div className="flex justify-end md:py-40 md:px-0  px-4 py-20">
+          <div className=" max-w-[1314px]">
+            <div>
+              <div>
+                <h3 className="text-white text-4xl font-light   leading-[44px] pb-8">
+                  Learn more about wine & art investment
+                </h3>
+              </div>
+            </div>
+
+            {/* here is new slider  section */}
+            <div className="hidden md:block">
+              <div>
+                <AboutSlider data={VideoSlider} />
+              </div>
+            </div>
+            <div className="md:hidden block  ">
+              <div className="grid grid-cols-1 ">
+                <AboutSlider />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* desktop */}
+      <div>
+        <NewslettersTwo />
+      </div>
+      <div className="py-40 bg-[#F7EFDF]">
+        <div className="container mx-auto px-4">
+          <div className="grid grid-cols-2 gap-8">
+            <div className="max-w-[498px]">
+              <h3 className=" text-zinc-800 text-4xl font-light leading-[44px]">
+                You might have questions, we definitely have answers.
+              </h3>
+              <div className="flex gap-3 pt-9">
+                <button className="border-b-2 border-orange-700 border-opacity-20 text-center text-orange-700 text-xs font-normal font-['Albert Sans'] uppercase leading-[18px] tracking-tight">
+                  SEE ALL QUESTIONS & ANSWERS
+                </button>
+                <NextIcon />
+              </div>
+            </div>
+
+            <div>
+              <AccordionWineAbout />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <Footer />
     </div>
   );
 }
---
+++ src/components/AssetsPerformance/TestimonialsSection.tsx
@@ -0,0 +1,49 @@
+import Image from "next/image";
+import React from "react";
+import videoplay from "@/assets/images/winevideobutton.svg";
+
+export default function TestimonialsSection() {
+  return (
+    <div>
+      <div className="grid grid-cols-12 gap-8">
+        <div className="col-span-5 flex flex-col justify-end items-end ">
+          <h3 className="max-w-[499px] text-center text-zinc-800 text-4xl font-light leading-[44px] pb-12">
+            Why fine wine is a unique investment asset class
+          </h3>
+          <div className="max-w-[501px] h-[299px] bg-red-800 pt-14 pl-12 pe-11 pb-10 relative">
+            <h1 className="text-center text-red-400 text-[144px] font-normal absolute top-0 leading-[90px]">
+              “
+            </h1>
+            <div>
+              <p className="text-rose-200 text-base font-normal leading-snug">
+                “Buying shares in a company is risky: management can fail,
+                reputations can be damaged, and competitors can disrupt its
+                entire business model.
+                <br />
+                <br />
+                Wine, on the other hand, is blissfully simple. Once bottled and
+                reviewed by critics, you already know enough to significantly
+                minimise your risk.
+              </p>
+              <br />
+              <p className="text-rose-200 text-lg font-semibold leading-relaxed">
+                Jason Zweig
+              </p>
+            </div>
+            <h1 className="text-end text-red-400 text-[144px] font-normal">
+              <span className="rotate-180 absolute right-10 -bottom-16">“</span>
+            </h1>
+          </div>
+        </div>
+
+        <div className="col-span-7 w-full md:h-[480px]  h-[480px] bg-no-repeat bg-cover bg-center bg-[url('https://i.ibb.co/j3xH23h/winevideo.png')] relative">
+          <div className="h-[480px] justify-center mx-auto items-center gap-8 flex flex-wrap md:px-0 px-4">
+            <div className="md:max-w-[124.04px] max-w-[90px] md:h-[124.12px] relative">
+              <Image src={videoplay} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
