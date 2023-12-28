33d1785bf93e62af19ff5b2ca0b3c24385844461
tauqeer180 <tauqeermalik855@gmail.com>
2023-12-28 10:48:10 +0500

    Update InfoTooltip.tsx

--- src/common/InfoTooltip.tsx
+++ src/common/InfoTooltip.tsx
@@ -6,7 +6,7 @@ export default function InfoTooltip({ fill = "#BF4D20" }: { fill?: any }) {
     <>
       <span className="group relative">
         <InfoIcon fill={fill} />
-        <div className=" hidden group-hover:block absolute w-40 px-4 py-3 mb-7 -ml-32 text-white bg-white shadow-md border rounded-lg z-10 top-[-125px] left-12">
+        <div className=" hidden group-hover:block absolute w-40 px-4 py-3 mb-7  text-white bg-white shadow-md border rounded-lg z-10 top-[-125px] -translate-x-[47%]">
           <span className="inline-block text-xs text-center font-normal leading-tight text-[#141619]  tracking-wide ">
             A quick explanation of the technology used here and how it differs
             from the {`"old word"`}. BLE explanation.
