68a697da4d3652e5d617054fc62850cd74a75ba1
Hsn abdullah <129732338+hsnabdullah@users.noreply.github.com>
2023-12-28 18:54:51 +0500

    update

--- src/common/InfoTooltip.tsx
+++ src/common/InfoTooltip.tsx
@@ -1,11 +1,18 @@
 import { InfoIcon } from "@/assets/icons/Icons";
 import React from "react";
 
-export default function InfoTooltip({ fill = "#BF4D20" }: { fill?: any }) {
+export default function InfoTooltip({
+  fill = "#BF4D20",
+  icon = <InfoIcon fill={fill} />,
+}: {
+  fill?: any;
+  icon?: any;
+}) {
   return (
     <>
       <span className="group relative">
-        <InfoIcon fill={fill} />
+        {/* <InfoIcon fill={fill} /> */}
+        {icon}
         <div className=" hidden group-hover:block absolute w-40 px-4 py-3 mb-7  text-white bg-white shadow-md border rounded-lg z-10 top-[-125px] -translate-x-[47%]">
           <span className="inline-block text-xs text-center font-normal leading-tight text-[#141619]  tracking-wide ">
             A quick explanation of the technology used here and how it differs
