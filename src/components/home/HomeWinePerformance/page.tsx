eebd0be48b60a69a039a33f62f481d93e5889bbd
CodeCork <143612150+CodeCork@users.noreply.github.com>
2024-01-03 13:33:48 +0100

    Finished animation

--- src/components/home/HomeWinePerformance/page.tsx
+++ src/components/home/HomeWinePerformance/page.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const items = [
   { name: 'Château Cheval Blanc 2019', photo: 'home_graph_cheval_blanc_bg.png', legend: 'Cheval Blanc', cover: 'home_cheval_blanc.png' },
@@ -15,13 +15,27 @@ export default function HomeWinePerformance() {
 
   const selectedItem = items[selectedTab]
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      let next = selectedTab + 1
+      if (next > 2) {
+        next = 0
+      }
+      setSelectedTab(next)
+
+    }, 6000)
+
+    return () => clearInterval(interval)
+
+  }, [selectedTab, setSelectedTab])
+
   return (
     <div className="md:py-40 md:px-0  py-16 px-4 bg-[#F7EFDF]">
       <div className="container mx-auto text-center">
         <h2 className="text-zinc-800 text-center md:text-7xl font-normal md:leading-[84px] md:tracking-[-1.44px] text-4xl leading-[42px] md:tracking[-0.36px]">
           Wine performance
         </h2>
-        <h3 className="mt-10">Between 2004 and 2022, fine wine grew at a 9% Compound Annual Growth Rate (CAGR)*</h3>
+        <h3 className="mt-10 text-xl">Between 2004 and 2022, fine wine grew at a 9% Compound Annual Growth Rate (CAGR)*</h3>
         <h4 className="mt-5 text-sm">*Liv-Ex 1000 Fine Wine Index</h4>
 
         <div className="w-full mt-10">
